11dd8a9681ea0cd9a43fd523e1a9b2d8146d4eb1
Sanika03 <sanikasuryawanshi0305@gmail.com>
2025-02-13 16:42:28 +0530

    Remove ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "./components/dashboard/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,14 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        {children}
       </body>
     </html>
   );
